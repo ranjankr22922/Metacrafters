379207a2f71b60cb31d3643dd0f3b31471908167
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-06-13 00:54:45 +0530

    Create eth_beginner.sol

---
+++ eth_beginner.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+
+// contract HelloWorld{
+//     string public myString = "hello world";
+// }
+
+
+// contract Storage{
+
+//     uint number;
+
+//     // takes a value _number and assigns it to the state variable number
+//     function setNumber(uint _number) public {
+//         number = _number;
+//     }
+
+//     // returns the value of the state variable number
+//     function getNumber() public view returns (uint) {
+//         return number;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// Data types - values and references
+
+// contract ValueTypes{
+
+//     bool public b = true;
+
+//     uint public u = 123;    //  uint = uint256, range = 0 to 2**256 - 1
+//                             //          uint8, range = 0 to 2**8 - 1
+//                             //          uint16, range = 0 to 2**16 - 1
+
+//     int public i = -123;    //  int = int256, range = -2**255 to 2**255 - 1
+//                             //          int128, range = -2**127 to 2**127 - 1
+//     int public minInt = type(int).min;
+//     int public maxInt = type(int).max;
+
+//     address public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+//     bytes32 public b32 = 0x89c58ced8a9078bdef2bb60f22e58eeff7dbfed6c2dff3e7c508b629295926fa;
+// }
+
+// contract Constants{     //constants will always be on uppercase letters
+//     address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc; //373 gas
+//     uint public constant MY_UINT = 123;
+// }
+
+// contract Var{
+//     address public MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;  //2483 gas
+// }
