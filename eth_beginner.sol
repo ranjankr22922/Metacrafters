ba4b24b02588184aad104f8d4da2fcbaed07ef82
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-06-13 04:37:33 +0530

    Update eth_beginner.sol

--- eth_beginner.sol
+++ eth_beginner.sol
@@ -52,6 +52,7 @@ pragma solidity 0.8.26;
 //     address public MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;  //2483 gas
 // }
 
+
 // Arrays
 // contract Array{
 //     uint[] public nums = [1,2,3];                 //dynamic
@@ -79,3 +80,41 @@ pragma solidity 0.8.26;
 // }
 
 
+// // Structs
+// contract Structs{
+//     struct Car{
+//         string model;
+//         uint year;
+//         address owner;
+//     }
+
+//     Car public car;
+//     Car[] public cars;
+//     mapping (address => Car[]) public carsByOwner;
+
+//     function examples() external {
+//         Car memory toyota = Car("Toyota", 1990, msg.sender);
+//         Car memory lambo = Car({model: "Lamborghini", year: 1980, owner: msg.sender});
+//         Car memory tesla;
+//         tesla.model = "Tesla";
+//         tesla.year = 2010;
+//         tesla.owner = msg.sender;
+
+//         cars.push(toyota);
+//         cars.push(lambo);
+//         cars.push(tesla);
+
+//         cars.push(Car("Ferrari", 2020, msg.sender));
+
+//         Car memory _car = cars[0];
+//         _car.model;
+//         _car.year;
+//         _car.owner;
+
+//         Car storage car = cars[0];
+//         _car.year = 1999;
+
+//         delete _car.owner;
+//         delete cars[1];
+//     }
+// }
