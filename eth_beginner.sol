76ec9a7613dfbd2a39239d54a725ea7b82d93f74
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-06-13 19:58:35 +0530

    Update eth_beginner.sol

--- eth_beginner.sol
+++ eth_beginner.sol
@@ -203,3 +203,15 @@ pragma solidity 0.8.26;
 //         return s;
 //     }
 // }
+
+
+// // Function
+// contract FunctionIntro{
+//     function add(uint x, uint y) external pure returns (uint){
+//         return x+y;   
+//     }
+
+//     function sub(uint x, uint y) external pure returns (uint){
+//         return x-y;   
+//     }
+// }
