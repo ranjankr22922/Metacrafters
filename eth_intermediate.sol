d2c627bba0250829e067e465a11aa698124db4a4
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-06-19 10:31:07 +0530

    Update eth_intermediate.sol

--- eth_intermediate.sol
+++ eth_intermediate.sol
@@ -1,39 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract greatestnum{
+// contract greatestnum{
 
-    uint a;
-    uint b;
-    uint c;
-    uint d;
-    address owner;
+//     uint a;
+//     uint b;
+//     uint c;
+//     uint d;
+//     address owner;
 
-    constructor() {
-        owner = msg.sender;
-    }
+//     constructor() {
+//         owner = msg.sender;
+//     }
     
-    modifier onlyOwner{
-        require(owner == msg.sender, "Can Only be accessed by the Owner.");
-        _;
-    }
+//     modifier onlyOwner{
+//         require(owner == msg.sender, "Can Only be accessed by the Owner.");
+//         _;
+//     }
 
-    function set(uint _a, uint _b, uint _c, uint _d) public onlyOwner{
-        a=_a;
-        b=_b;
-        c=_c;
-        d=_d;
-    }
+//     function set(uint _a, uint _b, uint _c, uint _d) public onlyOwner{
+//         a=_a;
+//         b=_b;
+//         c=_c;
+//         d=_d;
+//     }
 
-    function get() public view onlyOwner returns(uint){
-        if((a>b) && (a>c) && (a>d)){
-            return (a);
-        }
-        else if ((b>c) && (b>d) && (b>a)){
-            return (b);
-        }
-        else{
-            return (d);
-        }
-    }
-}
+//     function get() public view onlyOwner returns(uint){
+//         if((a>b) && (a>c) && (a>d)){
+//             return (a);
+//         }
+//         else if ((b>c) && (b>d) && (b>a)){
+//             return (b);
+//         }
+//         else{
+//             return (d);
+//         }
+//     }
+// }
