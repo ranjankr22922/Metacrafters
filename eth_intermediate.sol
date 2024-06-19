04706a15ddb93662dd8927ddd0df849ce4491271
Ranjan Kumar <125653560+ranjankr22922@users.noreply.github.com>
2024-06-19 10:24:02 +0530

    Create eth_intermediate.sol

---
+++ eth_intermediate.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract greatestnum{
+
+    uint a;
+    uint b;
+    uint c;
+    uint d;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner{
+        require(owner == msg.sender, "Can Only be accessed by the Owner.");
+        _;
+    }
+
+    function set(uint _a, uint _b, uint _c, uint _d) public onlyOwner{
+        a=_a;
+        b=_b;
+        c=_c;
+        d=_d;
+    }
+}
